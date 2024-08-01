69fcb84ecdf72e5ed5167f94c0a8462e61acf65e
kodi1993 <Akhmedka1993@gmail.com>
2024-08-01 13:06:24 +0300

    Redirect URLS, create base.html, adjust index.html and create contacts.html

--- marketplace/core/urls.py
+++ marketplace/core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
-    path('', 'index.html'),
+    path('', views.index, name='index'),
+    path('contacts/', views.contacts, name='contacts')
 ]
