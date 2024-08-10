3a95f2bc4ba9a18a71fe4b2410c91029a856b309
kodi1993 <Akhmedka1993@gmail.com>
2024-08-10 13:58:01 +0300

    Set up the Main Page

--- marketplace/core/urls.py
+++ marketplace/core/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from core.views import index, contacts
 
 urlpatterns = [
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
